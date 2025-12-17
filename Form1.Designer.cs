890c213b8684d66d1d5a3c099366e468a7513a45
antjoz <antonijozefowicz@icloud.com>
2025-12-17 12:39:41 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Bluetooth3._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.listDevices = new System.Windows.Forms.ListBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSendFile = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(25, 45);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(134, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Szukaj urządzeń Bluetooth";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // listDevices
+            // 
+            this.listDevices.FormattingEnabled = true;
+            this.listDevices.Location = new System.Drawing.Point(25, 74);
+            this.listDevices.Name = "listDevices";
+            this.listDevices.Size = new System.Drawing.Size(132, 134);
+            this.listDevices.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(25, 214);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(134, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Połącz z urządzeniem";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSendFile
+            // 
+            this.btnSendFile.Location = new System.Drawing.Point(25, 243);
+            this.btnSendFile.Name = "btnSendFile";
+            this.btnSendFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSendFile.TabIndex = 3;
+            this.btnSendFile.Text = "Wyślij plik";
+            this.btnSendFile.UseVisualStyleBackColor = true;
+            this.btnSendFile.Click += new System.EventHandler(this.btnSendFile_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(23, 20);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(77, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Status: gotowy";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(188, 277);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnSendFile);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.listDevices);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox listDevices;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSendFile;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+using System.Windows.Forms;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System.IO;
+
+namespace Bluetooth3._1
+{
+    public partial class Form1 : Form
+    {
+
+        private BluetoothClient bluetoothClient;
+        private BluetoothDeviceInfo[] devices;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bluetoothClient = new BluetoothClient();
+            lblStatus.Text = "Status: Bluetooth uruchomiony";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            listDevices.Items.Clear();
+            lblStatus.Text = "Status: wyszukiwanie urządzeń...";
+
+            try
+            {
+                devices = bluetoothClient.DiscoverDevices();
+
+                foreach (BluetoothDeviceInfo device in devices)
+                {
+                    listDevices.Items.Add(device.DeviceName);
+                }
+
+                lblStatus.Text = "Status: znaleziono " + devices.Length + " urządzeń";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd wyszukiwania: " + ex.Message);
+                lblStatus.Text = "Status: błąd";
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (listDevices.SelectedIndex == -1)
+            {
+                MessageBox.Show("Wybierz urządzenie z listy!");
+                return;
+            }
+
+            BluetoothDeviceInfo device = devices[listDevices.SelectedIndex];
+
+            try
+            {
+                bool paired = BluetoothSecurity.PairRequest(
+                    device.DeviceAddress,
+                    null // brak PIN – system zapyta automatycznie
+                );
+
+                if (paired)
+                    lblStatus.Text = "Status: połączono z " + device.DeviceName;
+                else
+                    lblStatus.Text = "Status: nie udało się połączyć";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd połączenia: " + ex.Message);
+            }
+        }
+
+        private void btnSendFile_Click(object sender, EventArgs e)
+        {
+            if (listDevices.SelectedIndex == -1)
+            {
+                MessageBox.Show("Wybierz urządzenie!");
+                return;
+            }
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Wybierz plik do wysłania";
+
+            if (ofd.ShowDialog() != DialogResult.OK)
+                return;
+
+            BluetoothDeviceInfo device = devices[listDevices.SelectedIndex];
+            Guid serviceClass = BluetoothService.ObexObjectPush;
+
+            try
+            {
+                using (BluetoothClient client = new BluetoothClient())
+                {
+                    client.Connect(device.DeviceAddress, serviceClass);
+
+                    using (Stream stream = client.GetStream())
+                    using (FileStream fs = File.OpenRead(ofd.FileName))
+                    {
+                        fs.CopyTo(stream);
+                    }
+                }
+
+                lblStatus.Text = "Status: plik wysłany poprawnie";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd wysyłania pliku: " + ex.Message);
+                lblStatus.Text = "Status: błąd wysyłania";
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            bluetoothClient?.Dispose();
+        }
+    }
+}
